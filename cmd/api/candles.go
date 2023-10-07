e6714311e4a1743834b7cde34d34064ee5c6944e
Aizhan Maratkyzy <73554654+Aizhan-creator@users.noreply.github.com>
2023-10-07 23:53:33 +0600

    4 chapter

--- cmd/api/candles.go
+++ cmd/api/candles.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"greenlight/internal/data"
 	"net/http"
@@ -9,6 +10,20 @@ import (
 
 func (app *application) createCandleHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "create a new candle")
+}
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+
+	var input struct {
+		Name        string  `json:"name"`
+		Description string  `json:"description"`
+		Price       float64 `json:"price,omitempty"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showCandleHandler(w http.ResponseWriter, r *http.Request) {
