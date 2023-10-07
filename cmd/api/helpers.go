abe079c792f15725e99a6f00f010b775cd236ef8
Aizhan Maratkyzy <73554654+Aizhan-creator@users.noreply.github.com>
2023-10-07 23:37:17 +0600

    3.3.2 chapter

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -8,20 +8,17 @@ import (
 	"strconv"
 )
 ...
-
 func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
-
-	js, err := json.Marshal(data)
+	// Use the json.MarshalIndent() function so that whitespace is added to the encoded
+	// JSON. Here we use no line prefix ("") and tab indents ("\t") for each element.
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
-
 	js = append(js, '\n')
-
 	for key, value := range headers {
 		w.Header()[key] = value
 	}
-
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	w.Write(js)
