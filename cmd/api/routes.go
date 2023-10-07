41809261e3a2e992c71520a3cfd9b9199fa683e6
Aizhan Maratkyzy <73554654+Aizhan-creator@users.noreply.github.com>
2023-10-07 22:34:50 +0600

    2.2 chapter

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -13,8 +13,8 @@ func (app *application) routes() *httprouter.Router {
 	// http.MethodPost are constants which equate to the strings "GET" and "POST"
 	// respectively.
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/candles", app.createCandleHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/candles/:id", app.showCandleHandler)
 	// Return the httprouter instance.
 	return router
 }
