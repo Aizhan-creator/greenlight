a92ae8a84401e391ac881d292864ec34d83dfb1f
Aizhan Maratkyzy <73554654+Aizhan-creator@users.noreply.github.com>
2023-10-12 13:33:01 +0600

    5.1 chapter

--- internal/data/candles.go
+++ internal/data/candles.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"encoding/json"
 	"time"
 )
 
@@ -10,4 +11,21 @@ type Candle struct {
 	Name        string    `json:"name"`
 	Description string    `json:"description"`
 	Price       float64   `json:"price,omitempty"`
+}
+
+func (c Candle) MarshalJSON() ([]byte, error) {
+	aux := struct {
+		ID          int64   `json:"id"`
+		Name        string  `json:"name"`
+		Description string  `json:"description"`
+		Price       float64 `json:"price,omitempty"`
+	}{
+		// Set the values for the anonymous struct.
+		ID:          c.ID,
+		Name:        c.Name,
+		Description: c.Description,
+		Price:       c.Price,
+	}
+	// Encode the anonymous struct to JSON, and return it.
+	return json.Marshal(aux)
 }
